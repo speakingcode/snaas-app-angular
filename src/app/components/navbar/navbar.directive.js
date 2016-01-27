77005fbd6a99c35eea29b3738c05d79cfa4a1675
speakingcode <dlissner@gmail.com>
2016-01-27 16:33:41 -0600

    Fix formatting in navbar html

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -23,12 +23,46 @@
     function NavbarController($scope, favoriteAlbumsSvc) {
       var vm = this;
       
+      vm.sections = [
+        {
+          name    : 'home',
+          label   : 'Home',
+          active  : true,
+          sref    : 'home'
+        },
+        {
+          name    : 'names',
+          label   : 'Names',
+          active  : false,
+          sref    : 'names'
+        },
+        {
+          name    : 'albums',
+          label   : 'Albums',
+          active  : false,
+          sref    : 'albums'
+        },
+        {
+          name    : 'trees',
+          label   : 'Trees',
+          active  : false,
+          sref    : 'trees'
+        },
+        {
+          name    : 'doggpound',
+          label   : 'Doggpound',
+          active  : false,
+          sref    : 'doggpound'
+        }
+      ];
+
       vm.count = favoriteAlbumsSvc.getFavoriteCount();
 
       $scope.$watch(
           function() { return favoriteAlbumsSvc.getFavoriteCount(); },
           function(newCount) { vm.count = newCount; }
       );
+
     }
   }
 
