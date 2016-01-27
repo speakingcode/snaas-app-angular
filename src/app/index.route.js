c00585ba0551e48dad70f4c27f73a7c58a759000
speakingcode <dlissner@gmail.com>
2016-01-27 02:13:03 -0600

    Add route for albums

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,7 +13,23 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
-      });
+      })
+
+    .state('albums', {
+      url: '/albums',
+      templateUrl: 'app/albums/albums.html',
+      controller: 'AlbumsController',
+      controllerAs: 'albums'
+    })
+
+    .state('albums.detail', {
+      url: '/albums/:id',
+      templateUrl: 'app/albums/albumDetail.html',
+      controller: 'AlbumDetailController',
+      controllerAs: 'albumDetail'
+    })
+    
+    ;
 
     $urlRouterProvider.otherwise('/');
   }
